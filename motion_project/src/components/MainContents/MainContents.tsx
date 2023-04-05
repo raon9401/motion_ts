c73c9e6874ad46b49741dd0007a403e1efc1dc73
raon9401 <raon9401@naver.com>
2023-04-05 14:08:34 +0900

    design : 버튼 레이아웃 디자인

---
+++ motion_project/src/components/MainContents/MainContents.tsx
@@ -0,0 +1,9 @@
+import { CreateBtnNav } from "./CreateNav/CreateBtnNav";
+
+export const MainContents = () => {
+  return (
+    <section className="main_layout">
+      <CreateBtnNav />
+    </section>
+  );
+};
---
+++ motion_project/src/constants/CreateBtnList.ts
@@ -0,0 +1,1 @@
+export const CREATE_BTN_LIST: string[] = ["Image", "Video", "Note", "TODO"];
